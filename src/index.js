9ca7d75fe7f647e49125ec1208f4ded001e88e89
Rohini TD <rohinitd@gmail.com>
2021-03-08 20:33:19 -0800

    Added script to publish tic-tac-toe widget in npm package.
     -> Added script to publish tic-tac-toe widget in npm package.
     -> Added boardmanager component.
     -> Display next player's move.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-let storeBoardData = JSON.parse(localStorage.getItem('boardData')) || null ;
-let storeNextPlayer = localStorage.getItem('nextMove') || 'X';
-
-ReactDOM.render(<App storeBoardData={storeBoardData} storeNextPlayer={storeNextPlayer} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
